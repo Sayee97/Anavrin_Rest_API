bcd05e56cf4c0d77fbdc5f680b27b39d03d199c8
Sayee97 <sayee0612@gmail.com>
2020-01-29 19:10:20 -0800

    main files

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_restful import Api,reqparse
 from flask_jwt import JWT,jwt_required,timedelta
 from security import authenticate, identity
 from resources.user import UserRegister
-from db import db
 from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 
@@ -13,9 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='jose'
 api=Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
 
 
 # app.config['JWT_AUTH_URL_RULE'] = '/login' 
@@ -34,5 +30,6 @@ api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores')
 
 if __name__=='__main__':
+	from db import db
 	db.init_app(app)
 	app.run(port=5000,debug=True)
